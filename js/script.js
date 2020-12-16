7cd8b27f04e0cc30c1b7e9bfd816c1511043907e
Sara <saragorleta@yahoo.com>
2020-12-16 15:03:54 +0100

    add js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
-alert('');
+// Generare un numero random da 1  a 6, sia per il giocatore
+//sia per il computer.
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+// document.getElementById("genera").addEventListener('click',
+// function(){
+
+  var numeroUtente= prompt('inserisci il numero da 1 a 6');
+  var numeroPc=Math.floor(Math.random()*6) +1;
+  var risultato='sono uguali';
+
+
+
+
+  if (numeroUtente>numeroPc){
+    var numeroUtente;
+
+  }
+  else if (numeroPc>numeroUtente){
+    var numeroPc;
+
+  }
+  else(numeroUtente==numeroPc){
+    var risultato;
+  }
+
+  document.getElementById('Genera').innerHTML='';
